85d17dbbaa6b1d0562bc654e7cb48e5611d09e65
Milozap <mzapolski96@gmail.com>
2025-09-25 02:21:19 +0200

    Add type hints to transaction model methods

--- apps/transactions/tests/factories.py
+++ apps/transactions/tests/factories.py
@@ -1,6 +1,5 @@
 import factory
 
-from apps.categories.models import Category
 from apps.categories.tests.factories import CategoryFactory
 from apps.transactions.models import Transaction
 from apps.users.factories import SuperUserFactory
@@ -13,6 +12,5 @@ class TransactionFactory(factory.django.DjangoModelFactory):
     date = factory.Faker("date")
     amount = factory.Faker("pydecimal", left_digits=6, right_digits=2)
     description = factory.Faker("sentence")
-    type = factory.Iterator([t for (t, _) in Category.TYPE_CHOICES])
     user_id = factory.SubFactory(SuperUserFactory)
     category = factory.SubFactory(CategoryFactory)
